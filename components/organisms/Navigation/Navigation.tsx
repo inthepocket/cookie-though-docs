92ed4fde8668bf26119f58545745181268a51135
Lukas Cornille <lukas.cornille@inthepocket.com>
2022-06-04 18:26:23 +0200

    feat: animate mobile menu

--- components/organisms/Navigation/Navigation.tsx
+++ components/organisms/Navigation/Navigation.tsx
@@ -1,3 +1,5 @@
+import type { ChangeEvent } from 'react';
+
 import styles from './Navigation.module.css';
 
 import Header from '@molecules/Header';
@@ -5,6 +7,12 @@ import Menu from '@molecules/Menu';
 
 const Navigation = () => {
   const id = 'navigation-menu';
+
+  const handleChange = ({ target: { checked } }: ChangeEvent<HTMLInputElement>) => {
+    const body = document.body;
+
+    return checked ? (body.style.overflow = 'hidden') : body.removeAttribute('style');
+  };
 
   return (
     <>
@@ -14,6 +22,7 @@ const Navigation = () => {
         type="checkbox"
         aria-hidden
         role="button"
+        onChange={handleChange}
       />
       <nav className={styles.navigation}>
         <Header id={id} />
