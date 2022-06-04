59119508eff0fd925db5970e0ed29614c8e841e7
Lukas Cornille <lukas.cornille@inthepocket.com>
2022-06-04 12:13:39 +0200

    chore: add pre-commit with commitlint

---
+++ scripts/postinstall.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+set -e
+
+function precommit() {
+  if command -v pre-commit >/dev/null; then
+    if ! grep -q "https://pre-commit.com" "$(dirname "$0")/../.git/hooks/pre-commit"; then
+      echo "[postinstall-local.sh] Installing git hooks (pre-commit)"
+      pre-commit install
+      pre-commit install --hook-type pre-push
+      pre-commit install --hook-type commit-msg
+    fi
+  else
+    read -rp "Install pre-commit and its hooks? " installPreCommit
+    case "$installPreCommit" in
+    y | Y | yes | YES)
+      brew install pre-commit
+      echo "[postinstall-local.sh] Installing git hooks (pre-commit)"
+      pre-commit install
+      pre-commit install --hook-type pre-push
+      pre-commit install --hook-type commit-msg
+      ;;
+    *) ;;
+    esac
+  fi
+}
+
+function main() {
+  if [ "$NODE_ENV" == "production" ] || [ "$SYSTEM" == "build" ]; then
+    echo ""
+    echo "[postinstall-local.sh] CI detected. Skipping..."
+    echo ""
+    exit 0
+  fi
+
+  precommit
+}
+
+main
