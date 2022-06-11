41dfe4f73ad98be0c2dbb8e9fc3cf102feddc592
Lukas Cornille <lukas.cornille@inthepocket.com>
2022-06-11 13:15:52 +0200

    feat: add top hero gradient

--- components/atoms/Tagline/Tagline.tsx
+++ components/atoms/Tagline/Tagline.tsx
@@ -1,5 +1,5 @@
 import type { ReactHTML, ReactNode } from 'react';
-import { createElement } from 'react';
+import { forwardRef, createElement } from 'react';
 
 import styles from './Tagline.module.css';
 
@@ -9,8 +9,10 @@ interface Props {
   children: ReactNode;
 }
 
-const Tagline = ({ type, variant, children }: Props) => {
-  return createElement(type, { className: styles[`tagline-${variant}`] }, children);
-};
+const Tagline = forwardRef<HTMLElement, Props>(({ type, variant, children }, ref) => {
+  return createElement(type, { className: styles[`tagline-${variant}`], ref }, children);
+});
+
+Tagline.displayName = 'Tagline';
 
 export default Tagline;
